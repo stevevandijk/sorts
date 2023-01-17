7ae3259d56fd3e29d5800d225550f26948486a56
stevevandijk <83955379+stevevandijk@users.noreply.github.com>
2023-01-17 11:51:21 +0100

    Create insertionsort.py
    insertionsort split from quicksort

---
+++ insertionsort.py
@@ -0,0 +1,27 @@
+def insertionsort(k):
+    control = order(k)
+    place = 0
+    maxplace = len(k) -1
+    while control == 1:
+        place2 = place +1
+        left = k[place]
+        right = k[place2]
+        if left > right:
+            k[place] = right
+            k[place2] = left
+        place = place + 1
+        if place == maxplace:
+            place = 0
+        control = order(k)
+    return k
+def order(test_list):
+    temp = 0
+    i = 1
+    while i < len(test_list):
+        if(test_list[i] < test_list[i - 1]):
+            temp = 1
+        i += 1
+    if (temp == 0) :
+        return temp
+    else :
+        return temp
