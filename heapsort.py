403f05c215a9403e217d0226d65061c78a3f807b
Sem Woltring <84123962+Sem1301@users.noreply.github.com>
2023-01-17 12:20:23 +0100

    Update heapsort.py
    Eerste docstring met uitleg, de uitgebreide uitleg komt later

--- heapsort.py
+++ heapsort.py
@@ -3,10 +3,10 @@ import numpy as np
 
 def heap_sort(arr):
     n = len(arr)
-    # Build max heap
+    '''Hier wordt de zogenaamde max heap opgebouwd doormiddel van de heapify method'''
     for i in range(n, -1, -1):
         heapify(arr, n, i)
-    # Extract elements one by one
+    '''Hier wordt er per element bekeken of die moet worden geswapped'''
     for i in range(n - 1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]   # swap
         heapify(arr, i, 0)
