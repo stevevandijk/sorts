3e28d0f8e3b210e364dc8f3e423f88a3b213473b
Sem Woltring <84123962+Sem1301@users.noreply.github.com>
2023-01-17 12:16:00 +0100

    Update heapsort.py
    Het heapsort algoritme gebruikmakend van numpy

--- heapsort.py
+++ heapsort.py
@@ -1,4 +1,31 @@
-def heapsort(list):
-	return
-def heapify(list):
-	return
+import numpy as np
+
+
+def heap_sort(arr):
+    n = len(arr)
+    # Build max heap
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
+
+    return arr
+
+
+def heapify(arr, n, i):
+    top = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        top = left
+    if right < n and arr[top] < arr[right]:
+        top = right
+    if top != i:
+        arr[i], arr[top] = arr[top], arr[i]
+        heapify(arr, n, top)
+
+
+array = np.array([8, 9, 3, 2, 1, 3, 7, 8, 10, 11])
+print(heap_sort(array))
