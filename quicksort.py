18e7f9f3d0d48310a568f2c9a1014addbbc910f3
stevevandijk <83955379+stevevandijk@users.noreply.github.com>
2023-01-17 12:04:51 +0100

    Update quicksort.py
    made quicksort easy to read

--- quicksort.py
+++ quicksort.py
@@ -1,24 +1,24 @@
 import random
 
-def quicksort(k):
-    if order(k) == 0:
-        return k
+def quicksort(lista):
+    if order(lista) == 0: #order() returns 0 if the list is sorted so if order() returns 0 then you just return the list
+        return lista      
    else:
-        b = len(k)
-        c = random.randrange(0,b)
-        d = k[c]
+        length = len(lista)
+        randomplace = random.randrange(0,length) #here we take a random place in the list which will give use the variable at that place. We will use this variable to sort the list around.
+        rotationpoint = lista[randomplace]
         left = []
         right = []
-        for i in k:
-            if i > d:
+        for i in lista: #here we start using the rotation variable, if something is lower it will go to the left and if its higher it will go to the right.
+            if i > rotationpoint:
                 right.append(i)
             else:
                 left.append(i)
-        left = quicksort(left)
-        right = quicksort(right)
+        left = quicksort(left) #we sort the left again until its sorted
+        right = quicksort(right) #we sort the right again until its sorted
         for i in right:
-            left.append(i)
-        return left
+            left.append(i) #we add the right to the left 
+        return left #and return a sorted list
 
 def order(test_list):
     temp = 0
