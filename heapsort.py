6ec6b14054937d442a448e352402a8d7f886b46f
Sem Woltring <84123962+Sem1301@users.noreply.github.com>
2023-01-21 17:01:14 +0100

    Update heapsort.py
    Variabelen namen adequaat aangepast. De functie gaat nu goed om met verkeerde input. Uitleg van de code uitgebreid.

--- heapsort.py
+++ heapsort.py
@@ -1,44 +1,83 @@
 import numpy as np
 
-# Heapsort is een sorteeralgoritme gebaseerd op het onderling vergelijken van de elementen uit de lijst.
-# Het algoritme verdeeld de lijst in een ongesorteerde lijst en een gesorteerde lijst.
-# Het verkleint de ongesorteerde lijst door telkens het grooste element eruit te halen en die toe te voegen
-# aan de gesorteerde lijst.
-# Om dit te doen gebruikt heapsort een zogenaamde heap data structuur, in het bijzonder 'max-heap'
-# Max-heap is een binary tree dat voldoet aan de eigenschap dat de waarde op elke node groter of gelijk moet zijn
-# aan de waarde van zijn child-nodes.
-# Om de array om te zetten naar zijn max-heap, ondergaat de array het heapify proces.
-# Het heapify proces vergelijkt de nodes met zijn child-nodes en indien er niet wordt voldaan aan de bovengenoemde
-# eigenschap, wordt de node omgewisselt met zijn desbetreffende child-node.
-# Dit wordt gedaan totdat er niks meer hoeft worden omgewisselt.
-# Vervolgens worden het laatste en het eerste element element van de max-heap omgewisselt en wordt proces
-# herhaalt voor alle elementen uit de nieuwe behalve het laatste gesorteerde (grootste) element aan het einde van de lijst.
+DEBUG = True
 
-def heap_sort(arr):
-    n = len(arr)
-    # Hier wordt de zogenaamde max-heap opgebouwd doormiddel van de heapify method
-    for i in range(n, -1, -1):
-        heapify(arr, n, i)
-    # Hier wordt er per element bekeken of die moet worden geswapped
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]   # swap
-        heapify(arr, i, 0)
 
+def heap_sort(arr) -> list:
+    quantity = len(arr)
+
+    # Hier wordt achteruit gelooped door de elementen van de lijst, waarbij ini_parent de parent_idx wordt.
+    # Als het heapify proces is gedaan, staat het grootste element aan het begin van de array.
+    # (Het heapify proces zet een array om in zijn max-heap data-structuur)
+
+    for ini_parent in range(quantity, -1, -1):
+        heapify(arr, quantity, ini_parent)
+
+    # Nu de array is zijn max-heap staat wordt het eerste grootste element omgewisseld met het laatste element uit
+    # de array.
+    # Vervolgens wordt heapify weer aangeroepen, die vervolgens voor de overige ongesorteerde elementen
+    # hetzelfde proces herhaalt.
+    # Als dat is gedaan wordt het nogmaals herhaalt, totdat deze for loop alles heeft gesorteerd.
+
+    for idx in range(quantity - 1, 0, -1):
+        arr[idx], arr[0] = arr[0], arr[idx]
+        heapify(arr, idx, 0)
+
+    # print('Your sorted list:')
+    print(arr)
     return arr
 
 
-def heapify(arr, n, i):
-    top = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-    if left < n and arr[i] < arr[left]:
-        top = left
-    if right < n and arr[top] < arr[right]:
-        top = right
-    if top != i:
-        arr[i], arr[top] = arr[top], arr[i]
-        heapify(arr, n, top)
+def heapify(arr: list, quantity: int, ini_parent: int) -> None:
+
+    # Parent is de index van de parent-node, left_idx is de index van de left child-node en right_idx
+    # is de index van right child_node.
+
+    parent_idx = ini_parent
+    left_idx = 2 * ini_parent + 1
+    right_idx = 2 * ini_parent + 2
+
+    # Hier wordt gecheckt of de left child-node groter is dan de parent-node.
+    # Zo ja dan wordt de nieuwe parent index, de index van de left child-node.
+    # Ook wordt er gechekt of de index van de left child-node kleiner dan de hoogst mogelijk indexgetal van
+    # de array, namelijk quantity.
+
+    if left_idx < quantity and arr[ini_parent] < arr[left_idx]:
+        parent_idx = left_idx
+
+    # Dezelfde check wordt gedaan voor de right child-node. Echter kan het nu zo zijn dat de left child-node de
+    # nieuwe parent-node is geworden, dus daarom arr[parent] in plaats van arr[index].
+
+    if right_idx < quantity and arr[parent_idx] < arr[right_idx]:
+        parent_idx = right_idx
+
+    # Hier wordt er gecheckt of de parent-node is veranderd.
+    # Als dat zo is wordt de nieuwe parent-node, de rechter of de linker child-node afhankelijk van welke
+    # de nieuwe parent-node is geworden.
+    # Daarna roept de functie zichzelf weer aan, maar is de nieuwe ini_parent, parent_idx.
+
+    if parent_idx != ini_parent:
+        arr[ini_parent], arr[parent_idx] = arr[parent_idx], arr[ini_parent]
+        heapify(arr, quantity, parent_idx)
 
 
-array = np.array([8, 9, 3, 2, 1, 3, 7, 8, 10, 11])
-print(heap_sort(array))
+def validate_input(value: str) -> list:
+    items = value.split(' ')
+    ret = []
+    for item in items:
+        try:
+            ret.append(int(item))
+        except ValueError:
+            if DEBUG:
+                print("Invalid value [{}] ignoring...".format(item))
+    return ret
+
+
+def main() -> None:
+    input_arr = input('input your list: ')
+    arr = validate_input(input_arr)
+    heap_sort(np.array(arr))
+
+
+if __name__ == '__main__':
+    main()
